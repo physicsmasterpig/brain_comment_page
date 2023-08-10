8defc760086b8aa0c29c539aefc339b0a9ee7ffb
physicsmasterpig <physicsmp1217@gmail.com>
2023-08-10 21:07:47 +0900

    First Commit

---
+++ test_info_popup.js
@@ -0,0 +1,7 @@
+document.getElementById("test_info_popup_open_button").onclick = function() {
+        document.getElementById("test_info_popup").style.display="block";
+}
+   
+document.getElementById("test_info_popup_close_button").onclick = function() {
+        document.getElementById("test_info_popup").style.display="none";
+}   
