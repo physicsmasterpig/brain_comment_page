aa77b38aa4e5040cbd8030536a3aeb24998e23e2
shinsabu <yssys414@yonsei.ac.kr>
2024-03-16 20:38:47 +0900

    2024-03-16

--- python/test.py
+++ python/test.py
@@ -1,12 +1,12 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+import sys
+from PyQt5.QtWidgets import *
 
-scope = 'https://spreadsheets.google.com/feeds'
-json = 'C:/Project/brain_comment_page/python/driven-catalyst-411908-9c041a7750dd.json'
-credentials = ServiceAccountCredentials.from_json_keyfile_name(json, scope)
-gc = gspread.authorize(credentials)
-sheet_url = 'https://docs.google.com/spreadsheets/d/1-xp3axKvBFVAMDt0SpnW1oxtPH9lzlgSvEN-WZna2H0/edit#gid=0'
-doc = gc.open_by_url(sheet_url)
-worksheet = doc.worksheet('ID')
-cell_data = worksheet.acell('b2').value
-print(cell_data)
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+
+app = QApplication(sys.argv)
+window = MyWindow()
+window.show()
+app.exec_()
